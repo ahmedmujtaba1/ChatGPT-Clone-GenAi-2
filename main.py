07d932dd132202851a7b11352b158233081d3eaa
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-10-03 18:02:24 +0500

    ChatGpt-Clone

--- main.py
+++ main.py
@@ -1,75 +1,49 @@
 from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
 import openai
+from urllib.parse import quote_plus
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb+srv://deweshchopra:sUlBW0jV3bEzf6ef@cluster0.xovt6s4.mongodb.net/chatgpt"
+password = quote_plus('myhappyfamily1@')
+print(password)
+app.config["MONGO_URI"] = f"mongodb+srv://teamahmed:{password}@cluster0.nzwisqq.mongodb.net/?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 @app.route("/")
 def home():
     chats = mongo.db.chats.find({})
-    mychats = [chat for chat in chats]
-    print(mychats)
-    return render_template("index.html", mychats=mychats)
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats = myChats)
 
 @app.route("/api", methods=["GET", "POST"])
-def qna():
-    data = {"answer": "It looks like you entered a random string of characters. If there's something specific you would like to discuss or any questions you have, feel free to let me know, and I'll be happy to help!"}
-
+def qa():
     if request.method == "POST":
         print(request.json)
         question = request.json.get("question")
         chat = mongo.db.chats.find_one({"question": question})
         print(chat)
         if chat:
-            data = {"answer": chat['answer']}
+            data = {"question": question, "answer": f"{chat['answer']}"}
             return jsonify(data)
         else:
-            # Code for da-vinci-003, GPT 3
-
-            # response = openai.Completion.create(
-            #     model="text-davinci-003",
-            #     prompt="",
-            #     temperature=0.7,
-            #     max_tokens=256,
-            #     top_p=1,
-            #     frequency_penalty=0,
-            #     presence_penalty=0
-            # )
-            # print(response)
-            # data = {"question": question, "answer": response['choices'][0]['text']}
-            # mongo.db.chats.insert_one({"question": question, "answer": response['choices'][0]['text']})
-
-            # Code for gpt-3.5-turbo, GPT 3.5
-
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "user",
-                        "content": question
-                    }
-                ],
-                temperature=0.7,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0
-            )
+            response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=question,
+                    temperature=0.7,
+                    max_tokens=256,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                    )
             print(response)
-            data = {"question": question, "answer": response['choices'][0]['message']['content']}
-            mongo.db.chats.insert_one({"question": question, "answer": response['choices'][0]['message']['content']})
-            
-            # mongo.db.chats.insert_one({"question": question, "answer": "It looks like you entered a random string of characters. If there's something specific you would like to discuss or any questions you have, feel free to let me know, and I'll be happy to help!"})
-
-            # data = {"answer": "It looks like you entered a random string of characters. If there's something specific you would like to discuss or any questions you have, feel free to let me know, and I'll be happy to help!"}
-            
+            data = {"question": question, "answer": response["choices"][0]["text"]}
+            mongo.db.chats.insert_one({"question": question, "answer": response["choices"][0]["text"]})
             return jsonify(data)
-
+    data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
+        
     return jsonify(data)
 
-app.run(debug=True)
+app.run(debug=True, port=5001)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ module.exports = {
   content: ["./templates/*.html"],
   theme: {
     extend: {
-      colors: {
-        chatblack: {
-          50: '#343541',
-          100: '#202123'
-        }
+      colors:{
+        chatblack: {50: '#343541'}
       }
-    }
+    },
   },
   plugins: [],
 }
+
